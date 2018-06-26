6f5f998cfeb20f21b6ac241b5e97b78ab562b523
Jason Hang <jasonxhang@gmail.com>
2018-06-26 03:08:36 +0000

    automatically backed up by learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,3 +1,7 @@
-def current_age_for_birth_year(any_year)
-  puts "2003 - #{any_year}"
+def current_age_for_birth_year(birth_year)
+  2003 - birth_year
 end
+
+function currentAge(birthYear) {
+  return 2003 - birthYear 
+}
